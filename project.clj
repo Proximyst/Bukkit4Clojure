b0cff397831b7e98bcc4f6c12b8e50e2a677fafc
Mariell Hoversholm <proximyst@proximyst.com>
2017-04-29 11:34:15 +0200

    Add leiningen project file

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject bukkit4clojure "0.1.0-SNAPSHOT"
+  :description "A Clojure wrapper for Bukkit."
+  :url "https://github.com/Project-Coalesce/Bukkit4Clojure"
+  :license {:name "MIT License"
+            :url "https://github.com/Project-Coalesce/Bukkit4Clojure/blob/master/LICENSE"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :repositories {"spigot-repo" "https://hub.spigotmc.org/nexus/content/repositories/snapshots/"}
+  :main com.coalesce.core
+  :java-source-paths ["src"]
+  :source-paths ["src"]
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}
+             :provided {:dependencies [[org.spigotmc/spigot-api "1.11.2-R0.1-SNAPSHOT"]]}})
