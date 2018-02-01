4316ef995e834c1c49a7b5a4ccf70464805bba52
Proximyst <proximyst@proximyst.com>
2018-02-01 21:53:38 +0100

    Add Return instance and format

--- src/com/proximyst/bukkitforclojure/Return.java
+++ src/com/proximyst/bukkitforclojure/Return.java
@@ -3,41 +3,44 @@ package com.proximyst.bukkitforclojure;
 import java.io.PrintStream;
 import java.io.PrintWriter;
 
-public class Return extends Exception {
-    @Override
-    public void printStackTrace() {
-    }
+public class Return
+      extends Exception {
+  public static final Return INSTANCE = new Return();
 
-    @Override
-    public void printStackTrace(PrintStream s) {
-    }
+  @Override
+  public void printStackTrace() {
+  }
 
-    @Override
-    public void printStackTrace(PrintWriter s) {
-    }
+  @Override
+  public void printStackTrace(PrintStream s) {
+  }
 
-    @Override
-    public String toString() {
-        return "";
-    }
+  @Override
+  public void printStackTrace(PrintWriter s) {
+  }
 
-    @Override
-    public String getLocalizedMessage() {
-        return "Returned out of Clojure function.";
-    }
+  @Override
+  public String toString() {
+    return "";
+  }
 
-    @Override
-    public String getMessage() {
-        return "Returned out of Clojure function.";
-    }
+  @Override
+  public String getLocalizedMessage() {
+    return "Returned out of Clojure function.";
+  }
 
-    @Override
-    public synchronized Throwable getCause() {
-        return null;
-    }
+  @Override
+  public String getMessage() {
+    return "Returned out of Clojure function.";
+  }
 
-    @Override
-    public StackTraceElement[] getStackTrace() {
-        return new StackTraceElement[0];
-    }
+  @Override
+  public synchronized Throwable getCause() {
+    return null;
+  }
+
+  @Override
+  public StackTraceElement[] getStackTrace() {
+    return new StackTraceElement[0];
+  }
 }
